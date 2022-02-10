cf970aefef98c97570869f35e2128d52b3686095
vivek <vivek@creatingwinners.com>
2022-02-10 22:55:56 +0800

    another way of coroutine

--- 1.RetrofitCoroutine/app/src/main/java/com/vivek/retrofitcoroutine/MainActivity.kt
+++ 1.RetrofitCoroutine/app/src/main/java/com/vivek/retrofitcoroutine/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.vivek.retrofitcoroutine
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -9,19 +8,35 @@ import com.vivek.retrofitcoroutine.databinding.ActivityMainBinding
 import com.vivek.retrofitcoroutine.model.QuoteListDo
 import com.vivek.retrofitcoroutine.network.RequestAPI
 import com.vivek.retrofitcoroutine.network.RetroHelper
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val quotesApi = RetroHelper.getInstance().create(RequestAPI::class.java)
+
+        //second way
+        GlobalScope.launch(IO) {
+            val result = quotesApi.getQuotes()
+
+            withContext(Main) {
+                showResponse(result.body())
+            }
+        }
+
+        /* first way */
         // launching a new coroutine
+        /*
         runBlocking {
             launch {
                 val result = quotesApi.getQuotes()
@@ -29,12 +44,13 @@ class MainActivity : AppCompatActivity() {
                 showResponse(result.body())
             }
         }
+        */
     }
 
     private fun showResponse(body: QuoteListDo?) {
         val list = body?.results
-        var myQuotes:ArrayList<String> = ArrayList()
-        for(items in list!!){
+        var myQuotes: ArrayList<String> = ArrayList()
+        for (items in list!!) {
             myQuotes.add(items.content)
         }
         var arrayAdapter = ArrayAdapter(
