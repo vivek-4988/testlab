9a2949ebdefd78127f7e6119135399dba65dd0f1
vivek <vivek@creatingwinners.com>
2022-02-11 16:47:52 +0800

    simple fragment code added

--- MyFragmentLab/app/src/main/java/com/vivek/myfragmentlab/MainActivity.kt
+++ MyFragmentLab/app/src/main/java/com/vivek/myfragmentlab/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.vivek.myfragmentlab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.vivek.myfragmentlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+    fun firstBtn(view: View) {
+        //Simple way to add fragment into current activity
+        supportFragmentManager.beginTransaction().replace(R.id.frameContainer, FirstFragment())
+            .commit()
+    }
+
+    fun secondBtn(view: View) {
+        //Simple way to add fragment into current activity
+        supportFragmentManager.beginTransaction().replace(R.id.frameContainer, SecondFragment())
+            .commit()
     }
 }
