503102f472c6d9fdf3e23df3f224940abba1c812
vivek <vivek@creatingwinners.com>
2022-02-12 10:23:04 +0800

    one time and repeat alarm

--- AlarmTestLab/app/src/main/java/com/vivek/alarmtestlab/MainActivity.kt
+++ AlarmTestLab/app/src/main/java/com/vivek/alarmtestlab/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.vivek.alarmtestlab
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn:Button = findViewById(R.id.btnClick)
+        btn.setOnClickListener {
+            var intent :Intent = Intent(this,SecondActivity::class.java)
+
+            //for one time alarm
+            //var pi:PendingIntent = PendingIntent.getActivity(this,13,intent,PendingIntent.FLAG_ONE_SHOT)
+            var pi:PendingIntent = PendingIntent.getActivity(this,13,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            var alarm:AlarmManager= getSystemService(ALARM_SERVICE) as AlarmManager
+
+            //todo for simple alarm
+            //alarm.set(AlarmManager.ELAPSED_REALTIME,60,pi)
+
+            //todo for repeating alarm
+            alarm.setRepeating(AlarmManager.ELAPSED_REALTIME,SystemClock.elapsedRealtime()+60,60000,pi)
+
+        }
     }
 }
