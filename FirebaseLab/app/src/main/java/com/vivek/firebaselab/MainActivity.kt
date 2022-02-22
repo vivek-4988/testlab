af931836792f76699279168b8a63f0234e9db752
vivek <vivek@creatingwinners.com>
2022-02-23 02:37:08 +0800

    setting values in node and saving to realtime db

--- FirebaseLab/app/src/main/java/com/vivek/firebaselab/MainActivity.kt
+++ FirebaseLab/app/src/main/java/com/vivek/firebaselab/MainActivity.kt
@@ -15,14 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
+    //I selected different region for Database so otherwise no need to write this code
+    val regionReference = "https://fir-lab-9d8d4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    val dbRef = FirebaseDatabase.getInstance(regionReference).reference
     fun saveBtn(view: View) {
         var name = binding.edtName.text.toString()
         if (name.isNotEmpty()) {
-            //I selected different region for Database so otherwise no need to write this code
-            val regionReference = "https://fir-lab-9d8d4-default-rtdb.asia-southeast1.firebasedatabase.app/"
-            //upload name
-            FirebaseDatabase.getInstance(regionReference).reference.setValue(name)
-            //var dbReference =
+
+            //TODO 1. upload name to default root db
+            //FirebaseDatabase.getInstance(regionReference).reference.setValue(name)
+
+            //TODO 2. adding push will not overwrite the value
+            //FirebaseDatabase.getInstance(regionReference).reference.push().setValue(name)
+
+            //TODO 3. adding to particular nodes i.e. as child
+            //dbRef.child("node").push().setValue(name)
+            dbRef.child("todo").push().setValue(name)
 
         } else {
             binding.edtName.hint = "Please enter name"
