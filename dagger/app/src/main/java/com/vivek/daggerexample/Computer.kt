52f82b963408d572f5651b7d6664084baab3e930
vivek <vivek@creatingwinners.com>
2023-12-17 17:08:14 +0800

    constructor inject

--- dagger/app/src/main/java/com/vivek/daggerexample/Computer.kt
+++ dagger/app/src/main/java/com/vivek/daggerexample/Computer.kt
@@ -6,12 +6,28 @@ import javax.inject.Inject
 
 //@Inject is equal to val com = Computer()
 
-class Computer @Inject  constructor(){
+class Computer @Inject  constructor(var ram:RAM,var hd:HardDisk){
 
     fun getComputer(): Unit {
+        ram.getRam()
+        hd.getHardDisk()
         Log.d("main", "getComputer: ")
     }
 
 }
 
+class RAM @Inject  constructor(){
 
+    fun getRam(): Unit {
+        Log.d("main", "getRam: ")
+    }
+
+}
+
+class HardDisk @Inject  constructor(){
+
+    fun getHardDisk(): Unit {
+        Log.d("main", "getHardDisk: ")
+    }
+
+}
